47036f2e7bfb6314b820ba12d0a50983907ef7a0
Andy Chiang <andy10801@gmail.com>
2022-01-08 15:55:06 +0800

    start project&app

---
+++ web/admin.py
@@ -0,0 +1,58 @@
+from django.contrib import admin
+from .models import Department, Class, Profile, Comment
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class DepartmentResource(resources.ModelResource):
+    class Meta:
+        model = Department
+        fields = ('id', 'dDept')
+
+class DepartmentAdmin(ImportExportModelAdmin):
+    resource_class = DepartmentResource
+    list_display = ('id', 'dDept')
+    search_fields = ('dDept', )
+    ordering = ('dDept', 'id')
+
+admin.site.register(Department, DepartmentAdmin)
+
+
+class ClassResource(resources.ModelResource):
+    class Meta:
+        model = Class
+        fields = ('id', 'cCode', 'cName', 'cEn_name', 'cProfessor', 'cDept', 'cType', 'cCredit', 'cLang')
+
+class ClassAdmin(ImportExportModelAdmin):
+    resource_class = ClassResource
+    list_display = ('id', 'cCode', 'cName', 'cEn_name', 'cProfessor', 'cDept', 'cType', 'cCredit', 'cLang')
+    search_fields = ('id', 'cCode', 'cName', 'cEn_name', 'cProfessor', 'cDept', 'cType', 'cCredit', 'cLang')
+    ordering = ('id', )
+
+admin.site.register(Class, ClassAdmin)
+
+
+class ProfileResource(resources.ModelResource):
+    class Meta:
+        model = Profile
+
+class ProfileAdmin(ImportExportModelAdmin):
+    resource_class = ProfileResource
+    list_display = ('id', 'pUID', 'pDept')
+    search_fields = ('id', 'pUID', 'pDept')
+    ordering = ('id', )
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class CommentResource(resources.ModelResource):
+    class Meta:
+        model = Comment
+
+class CommentAdmin(ImportExportModelAdmin):
+    resource_class = CommentResource
+    list_display = ('id', 'mUID', 'mCID', 'mLasttime')
+    search_fields = ('id', 'mUID', 'mCID')
+    ordering = ('mLasttime', 'id')
+
+admin.site.register(Comment, CommentAdmin)
---
+++ web/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Department(models.Model):
+    dDept = models.CharField("系所名稱", max_length=50, unique=True)
+
+    def __str__(self):
+        return self.dDept
+
+
+class Class(models.Model):
+    cCode = models.CharField("課程代碼", max_length=4)
+    cName = models.CharField("課程中文名稱", max_length=50)
+    cEn_name = models.CharField("課程英文名稱", max_length=100, null=True)
+    cProfessor = models.CharField("課程教師", max_length=50)
+    cDept = models.CharField("課程系所", max_length=50)
+    cType = models.CharField("課程類別", max_length=2)
+    cCredit = models.DecimalField("課程學分", max_digits=1, decimal_places=0, default=0)
+    cLang = models.CharField("課程語言", max_length=2)
+    cCool = models.DecimalField("涼", max_digits=3, decimal_places=1, default=0)
+    cSweet = models.DecimalField("甜", max_digits=3, decimal_places=1, default=0)
+    cFun = models.DecimalField("有趣", max_digits=3, decimal_places=1, default=0)
+    cLearn = models.DecimalField("學習", max_digits=3, decimal_places=1, default=0)
+    cJoin = models.DecimalField("參與", max_digits=3, decimal_places=1, default=0)
+
+    def __str__(self):
+        return self.cName
+
+
+class Profile(models.Model):
+    pUID = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    pDept = models.ForeignKey(to=Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.pUID)
+
+
+class Comment(models.Model):
+    mUID  = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    mCID = models.ForeignKey(to=Class, on_delete=models.CASCADE)
+    mContent = models.CharField("評論內容", max_length=1000)
+    mCool = models.DecimalField("涼", max_digits=2, decimal_places=0, default=0)
+    mSweet = models.DecimalField("甜", max_digits=2, decimal_places=0, default=0)
+    mFun = models.DecimalField("有趣", max_digits=2, decimal_places=0, default=0)
+    mLearn = models.DecimalField("學習", max_digits=2, decimal_places=0, default=0)
+    mjoin = models.DecimalField("參與", max_digits=2, decimal_places=0, default=0)
+    mLasttime = models.DateTimeField("最後修改時間", default=timezone.now)
+
+    def __str__(self):
+        return f"{self.mUID}, {self.mCID}"
+
+    class Meta:
+        unique_together = (("mUID", "mCID"),)
