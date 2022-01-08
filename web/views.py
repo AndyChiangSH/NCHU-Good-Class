5b619dcf85fd08e98c3c12fd64874d677fcbc122
Andy Chiang <andy10801@gmail.com>
2022-01-08 18:37:52 +0800

    index complete

--- web/views.py
+++ web/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Department, Class, Profile, Comment
+from django.views import generic
+
 
 # Create your views here.
+def index(request):
+    
+    num_user = User.objects.all().count()
+    num_class = Class.objects.all().count()
+    num_comment = Comment.objects.all().count()
+
+    context = {
+        "num_user": num_user,
+        "num_class": num_class,
+        "num_comment": num_comment,
+    }
+
+    return render(request, "web/index.html", context)
+
+
+
+class ClassListView(generic.ListView):
+    model = Class
+    paginate_by = 20
