db8e0beeb8aab370992209135a0073fe5d4da2ce
Andy Chiang <andy10801@gmail.com>
2022-01-08 21:56:48 +0800

    0.1.2 - classes list complete

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('classes/', views.ClassListView.as_view(), name="classes"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Department, Class, Profile, Comment
@@ -20,7 +21,31 @@ def index(request):
     return render(request, "web/index.html", context)
 
 
-
 class ClassListView(generic.ListView):
     model = Class
+    template_name = "web/classes.html"
     paginate_by = 20
+    order_dy = ('code')
+
+    def get_queryset(self):
+        t = self.request.GET.get('t')
+        q = self.request.GET.get('q')
+
+        print(t)
+        print(q)
+
+        object_list = self.model.objects.none()
+
+        if t == None or q == None or q == "":
+            object_list = self.model.objects.all()
+        else:
+            if t == "code":
+                object_list = self.model.objects.filter(cCode__contains=q)
+            elif t == "name":
+                object_list = self.model.objects.filter(cName__contains=q)
+            elif t == "professor":
+                object_list = self.model.objects.filter(cProfessor__contains=q)
+            elif t == "dept":
+                object_list = self.model.objects.filter(cDept__contains=q)
+
+        return object_list
