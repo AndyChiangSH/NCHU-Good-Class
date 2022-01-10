5c24410fc34fe5feaef2d19eb8821ea44ede8ce2
Andy Chiang <andy10801@gmail.com>
2022-01-10 22:44:51 +0800

    0.1.12 - deploy 4th

--- nchugoodclass/settings.py
+++ nchugoodclass/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
